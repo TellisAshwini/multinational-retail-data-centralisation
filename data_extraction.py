9669fba4f49fd7ee224c68128b74dc60eaa21e5f
TellisAshwini <102413647+TellisAshwini@users.noreply.github.com>
2023-03-24 15:21:26 +0000

    Creates read_rds_table method

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine, text 
+import tabula
+import requests
+import json
+import os
+
 class DataExtractor:
-    pass
+    def read_rds_table(self, engine, table_name):
+        #engine is the instance of DatabaseConnector and table_name for 1st one is user table from the list
+        table = pd.DataFrame(engine.connect().execute(text(f'''select * from {table_name}'''))).set_index('index')
+        return table
