f5c9cd38a62c602d3dabe09a32767a73a556fb1d
TellisAshwini <102413647+TellisAshwini@users.noreply.github.com>
2023-03-24 15:18:50 +0000

    Adds read_db_creds method

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,15 @@
+import yaml
+from sqlalchemy import create_engine, text
+from sqlalchemy import inspect
+import data_extraction as de
+import data_cleaning as dc
+import time
+
 class DatabaseConnector:
-    pass
+    def __init__(self):
+        self.creds = {}
+    
+    def read_db_creds(self):
+        with open('db_creds.yaml') as yaml_file:
+            self.creds = yaml.safe_load(yaml_file)
+        return self.creds
