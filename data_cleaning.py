f9758f6cb5b78c6e09128b92fdf6f5db528a420f
TellisAshwini <102413647+TellisAshwini@users.noreply.github.com>
2023-03-24 15:29:40 +0000

    creates clean_card_data method

--- data_cleaning.py
+++ data_cleaning.py
@@ -13,3 +13,13 @@ class DataCleaning:
             if column in table.columns:
                 table[column] = table[column].apply(lambda x: pd.to_datetime(x, errors='coerce', infer_datetime_format= True))
         return table
+    
+    def clean_card_data(self, table):
+        #drops the rows with wrong data
+        table = table.drop(table[table['expiry_date'].apply(lambda x: (len(x) > 5) or len(x) == 4)].index.tolist())
+        # removes '?' from card_number
+        table['card_number'] = table['card_number'].apply(lambda x: str(x) )
+        table['card_number'] = table['card_number'].apply(lambda x: x.strip('?') if '?' in x else x)
+        #converts the date strings to ISO date format
+        table['date_payment_confirmed'] = table['date_payment_confirmed'].apply(lambda x: pd.to_datetime(x, errors='coerce', infer_datetime_format= True))
+        return table
